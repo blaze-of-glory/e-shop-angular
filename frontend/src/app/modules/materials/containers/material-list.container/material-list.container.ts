0117ee7161630d31b35f14062bd470ab763188fe
Mikhail Petrunenko <blazeofglory999@gmail.com>
2023-04-06 19:38:22 +0200

    remove redundant facade layer from materials module

--- frontend/src/app/modules/materials/containers/material-list.container/material-list.container.ts
+++ frontend/src/app/modules/materials/containers/material-list.container/material-list.container.ts
@@ -1,7 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Material } from '../../classes/material';
-import { MaterialsFacade } from '../../materials.facade';
 import { SubscriptionHelper } from '../../../../shared/helpers/subscription.helper';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { selectAllMaterials, selectCurrentMaterial, selectRelatedProviderId } from '../../store/materials.selectors';
+import { take } from 'rxjs';
+import { createMaterial, editMaterial, getMaterials, setCurrentMaterial } from '../../store/materials.actions';
+import { ROUTER_LINKS } from '../../../../shared/constants/router-links';
 
 @Component({
   selector: 'app-material-list.container',
@@ -13,14 +18,16 @@ export class MaterialListContainer implements OnInit, OnDestroy {
   public material: Material = null;
   private readonly subscriptionHelper: SubscriptionHelper = new SubscriptionHelper();
 
-  constructor(private facade: MaterialsFacade) { }
+  constructor(private store: Store, private router: Router) { }
 
   ngOnInit(): void {
-    this.facade.setMaterials();
-    this.subscriptionHelper.next = this.facade.getMaterials$().subscribe(materials => {
+    this.subscriptionHelper.next = this.store.select(selectRelatedProviderId).subscribe(relatedProviderId => {
+        this.store.dispatch(getMaterials({ relatedProviderId }));
+      });
+    this.subscriptionHelper.next = this.store.select(selectAllMaterials).subscribe(materials => {
       this.materials = materials;
     });
-    this.subscriptionHelper.next = this.facade.getCurrentMaterial$().subscribe(material => {
+    this.subscriptionHelper.next = this.store.select(selectCurrentMaterial).subscribe(material => {
       this.material = material;
     });
   }
@@ -30,23 +37,37 @@ export class MaterialListContainer implements OnInit, OnDestroy {
   }
 
   openMaterial(details: Material) {
-    this.facade.openProducts(details.id);
+    this.store.select(selectRelatedProviderId)
+      .pipe(take(1))
+      .subscribe(relatedProviderId => {
+        this.router.navigateByUrl(ROUTER_LINKS.PROVIDERS + `/${relatedProviderId}/${details.id}`);
+      });
   }
 
   addMaterial() {
-    this.facade.addMaterial();
+    this.setCurrentMaterial(new Material());
   }
 
-  setCurrentMaterial(material: Material) {
-    this.facade.setCurrentMaterial(material);
+  setCurrentMaterial(currentMaterial: Material) {
+    this.store.dispatch(setCurrentMaterial({ currentMaterial }));
   }
 
-  createMaterial(material: Material) {
-    this.facade.createMaterial(material);
+  createMaterial(currentMaterial: Material) {
+    this.store.select(selectRelatedProviderId)
+      .pipe(take(1))
+      .subscribe(relatedProviderId => {
+        this.store.dispatch(createMaterial({ currentMaterial, relatedProviderId }));
+        this.setCurrentMaterial(null);
+      });
   }
 
-  editMaterial(material: Material) {
-    this.facade.editMaterial(material);
+  editMaterial(currentMaterial: Material) {
+    this.store.select(selectRelatedProviderId)
+      .pipe(take(1))
+      .subscribe(relatedProviderId => {
+        this.store.dispatch(editMaterial({ currentMaterial, relatedProviderId }));
+        this.setCurrentMaterial(null);
+      });
   }
 
   isCreationMode(): boolean {
@@ -54,6 +75,6 @@ export class MaterialListContainer implements OnInit, OnDestroy {
   }
 
   cancel() {
-    this.facade.setCurrentMaterial(null);
+    this.setCurrentMaterial(null);
   }
 }
