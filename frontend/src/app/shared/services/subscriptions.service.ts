e1fe233960711bde235797a74f2f06ffa35104b8
Mikhail Petrunenko <blazeofglory999@gmail.com>
2023-05-05 17:48:35 +0300

    improve subscriptions service logic

--- frontend/src/app/shared/services/subscriptions.service.ts
+++ frontend/src/app/shared/services/subscriptions.service.ts
@@ -3,20 +3,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SubscriptionsService {
-    private subscriptions: Array<Subscription | (() => void)> = [];
+    private subscriptions: Subscription[] = [];
 
-    public set next(subscription: Subscription | (() => void)) {
+    public set next(subscription: Subscription) {
         this.subscriptions.push(subscription);
     }
 
     public unsubscribeAll(): void {
-        this.subscriptions.forEach(item => {
-            if (item instanceof Subscription) {
-                item.unsubscribe();
-            } else if (typeof item === 'function') {
-                item();
-            }
-        });
+        this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
 
         this.subscriptions = [];
     }
